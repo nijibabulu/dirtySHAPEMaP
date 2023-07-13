fa7b782dc7077a2b4de6247f193db46690858ebd
Bob Zimmermann <robert.paul.zimmermann@gmail.com>
2023-07-12 22:45:59 -0400

    histograms for lib 2

--- scripts/flash_histograms.R
+++ scripts/flash_histograms.R
@@ -1,20 +1,26 @@
 # required packages: readr, ggplot2
 
-df <-
-  readr::read_tsv("work/04_flash/lib_y7oDUzWh-01.hist",
-                col_names = c("Length", "Count"))
+generate_histograms <- function(base) {
+  df <-
+    readr::read_tsv(glue::glue("work/04_flash/{base}.hist"),
+                  col_names = c("Length", "Count"))
 
-linear_plot <-
-  ggplot2::ggplot(df, ggplot2::aes(Length, Count)) +
-  ggplot2::geom_col() +
-  ggplot2::labs(title = "lib_y7oDUzWh length histogram") +
-  ggplot2::theme_minimal()
+  linear_plot <-
+    ggplot2::ggplot(df, ggplot2::aes(Length, Count)) +
+    ggplot2::geom_col() +
+    ggplot2::labs(title = glue::glue("{base} length histogram")) +
+    ggplot2::theme_minimal()
 
-log_plot <- linear_plot +
-  ggplot2::scale_y_log10() +
-  ggplot2::labs(title = "lib_y7oDUzWh length histogram (log scale)")
+  log_plot <- linear_plot +
+    ggplot2::scale_y_log10() +
+    ggplot2::labs(title = glue::glue("{base} length histogram (log scale)"))
 
-ggplot2::ggsave("results/lib_y7oDUzWh-01/linear_histogram.png", linear_plot,
-                width = 5, height = 5)
-ggplot2::ggsave("results/lib_y7oDUzWh-01/log_histogram.png", log_plot,
-                width = 5, height = 5)
+  fs::dir_create(fs::path("results", base))
+  ggplot2::ggsave(fs::path("results", base, "linear_histogram.png"), linear_plot,
+                  width = 5, height = 5)
+  ggplot2::ggsave(fs::path("results", base, "log_histogram.png"), log_plot,
+                  width = 5, height = 5)
+}
+
+
+generate_histograms("lib_y7oDUzWh-02")
